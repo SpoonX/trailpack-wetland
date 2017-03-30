9f117210786d00a2344d02e2c0779bfe269720c6
Rawphs <raphaela@spoonx.nl>
2017-03-30 17:40:32 +0200

    chore(project): added config files

---
+++ config/trailpack.js
@@ -0,0 +1,11 @@
+/**
+ * Trailpack Configuration
+ *
+ * @see {@link http://trailsjs.io/doc/trailpack/config
+ */
+module.exports = {
+  provides: {
+    services: ['FootprintService', 'MigrationService']
+  },
+  lifecycle: {}
+};
